84fc951ad35860653ce34ec41aac1c7facc30c11
nkastratovic <nkastratovic@gmail.com>
2024-01-31 17:52:51 +0100

    Run Method.

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Run is predefined method to create middleware, in Run method define lambda expression to execute when request is received
+app.Run(async (HttpContext context) => {
+    await context.Response.WriteAsync("Middleware 1\n");
+});
 
 app.Run();
