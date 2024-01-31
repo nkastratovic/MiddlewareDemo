a958f59aa7d1af7c93fb96df0622ff0ecdb451f5
nkastratovic <nkastratovic@gmail.com>
2024-01-31 18:39:41 +0100

    Middleware Chain - Use Method.

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+//Middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) => {
+    await context.Response.WriteAsync("Middleware 1 Request\n");
+    await next(context);
+    await context.Response.WriteAsync("Middleware 1 Response\n");
+});
+
+//Middleware 2
+app.Use(async (HttpContext context, RequestDelegate next) => {
+    await context.Response.WriteAsync("Middleware 2 Request\n");
+    await next(context);
+    await context.Response.WriteAsync("Middleware 2 Response\n");
+});
+
+//Middleware 3
 //Run is predefined method to create middleware, in Run method define lambda expression to execute when request is received
 app.Run(async (HttpContext context) => {
-    await context.Response.WriteAsync("Middleware 1\n");
+    await context.Response.WriteAsync("Middleware 3\n");
 });
 
 app.Run();
