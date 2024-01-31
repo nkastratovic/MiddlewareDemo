c236c826ff953c63ce5810010e782922ca6d8221
nkastratovic <nkastratovic@gmail.com>
2024-01-31 18:46:55 +0100

    UseWhen Method.

--- src/Program.cs
+++ src/Program.cs
@@ -15,6 +15,17 @@ app.Use(async (HttpContext context, RequestDelegate next) => {
     await context.Response.WriteAsync("Middleware 2 Response\n");
 });
 
+//UseWhen Method
+app.UseWhen(
+    context => context.Request.Query.ContainsKey("username"),
+    app => {
+        app.Use(async (HttpContext context, RequestDelegate next) =>
+        {
+            await context.Response.WriteAsync("Query contains username\n");
+            await next(context);
+        });
+    });
+
 //Middleware 3
 //Run is predefined method to create middleware, in Run method define lambda expression to execute when request is received
 app.Run(async (HttpContext context) => {
